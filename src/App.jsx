9be1315fcb2cdcf672edb98361f867705bc2dde6
tan-nguyen <tannguyen24894@gmail.com>
2019-04-23 18:45:44 +0700

    Init project

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,8 @@
 import React, { Component } from 'react';
 import { Helmet } from 'react-helmet';
 import { BrowserRouter } from 'react-router-dom';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Router from './routes';
 import { addFont } from './fontAwesome';
-
-const theme = createMuiTheme({
-  typography: {
-    useNextVariants: true,
-    shadows: []
-  }
-});
-
-theme.breakpoints.values = {
-  xs: 0,
-  sm: 564,
-  md: 768,
-  lg: 948,
-  xl: 1020
-};
 
 addFont();
 
@@ -26,16 +10,10 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <MuiThemeProvider theme={theme}>
           <Helmet>
-            <link
-              rel="stylesheet"
-              href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
-            />
             <title>About Tan Nguyen</title>
           </Helmet>
           <Router />
-        </MuiThemeProvider>
       </BrowserRouter>
     );
   }
---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { Homepage } from '../pages';
+
+const Router = () => {
+  return (
+  <Switch>
+    <Route path="/" component={Homepage} />
+  </Switch>);
+};
+
+export default Router;
